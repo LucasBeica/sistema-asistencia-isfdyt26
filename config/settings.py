6ea14300f2a5a4f572964e4f26bb3ab694d528e1
Lucas Beica <lucas_beica04@hotmail.com>
2026-08-12 06:08:18 -0300

    Corregir ubicación de templates

--- config/settings.py
+++ config/settings.py
@@ -102,23 +102,14 @@ ROOT_URLCONF = "config.urls"
 # ==================================================
 
 TEMPLATES = [
-
     {
-        "BACKEND":
-            "django.template.backends.django.DjangoTemplates",
-
-        "DIRS": [],
-
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "Asistencia" / "templates"],
         "APP_DIRS": True,
-
         "OPTIONS": {
-
             "context_processors": [
-
                 "django.template.context_processors.request",
-
                 "django.contrib.auth.context_processors.auth",
-
                 "django.contrib.messages.context_processors.messages",
             ],
         },
